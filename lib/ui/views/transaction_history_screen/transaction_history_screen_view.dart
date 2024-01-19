852e4a227ed2dd2f0747b840842b0dd6a0f029e2
UMChannar <usamamuhammadchannar@gmail.com>
2024-01-19 12:19:24 +0500

    Designed transaction History Screen

--- lib/ui/views/transaction_history_screen/transaction_history_screen_view.dart
+++ lib/ui/views/transaction_history_screen/transaction_history_screen_view.dart
@@ -16,133 +16,131 @@ class TransactionHistoryScreenView
     TransactionHistoryScreenViewModel viewModel,
     Widget? child,
   ) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            'Transaction History',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 24,
-              fontWeight: FontWeight.w600,
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Transaction History',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
           ),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Divider(
-                height: 1,
-                color: Colors.black,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Divider(
+              height: 1,
+              color: Colors.black,
+            ),
+            Container(
+              padding: const EdgeInsets.all(20.0),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '02 Dec 2023',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  HomeTransactionRow(
+                    buttonColor: kcSuccessGreen,
+                    buttonText: 'Completed',
+                    btc: '+0.001',
+                    image: bitCoin,
+                    imageBack: kcYellowBright,
+                    btcColor: kcYellowBright,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  HomeTransactionRow(
+                    buttonColor: kcFailRed,
+                    buttonText: 'Failed',
+                    btc: '-0.001',
+                    image: bitCoin,
+                    imageBack: kcYellowBright,
+                    btcColor: kcYellowBright,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  HomeTransactionRow(
+                    buttonColor: kcPendingGrey,
+                    buttonText: 'Pending',
+                    btc: '-0.001',
+                    image: silver,
+                    imageBack: kcSilverLight,
+                    btcColor: kcSilverLight,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  HomeTransactionRow(
+                    buttonColor: kcPendingGrey,
+                    buttonText: 'Pending',
+                    btc: '-0.001',
+                    image: silver,
+                    imageBack: kcSilverLight,
+                    btcColor: kcSilverLight,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    '02 Dec 2023',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  HomeTransactionRow(
+                    buttonColor: kcSuccessGreen,
+                    buttonText: 'Completed',
+                    btc: '+0.001',
+                    image: silver,
+                    imageBack: kcSilverLight,
+                    btcColor: kcSilverLight,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  HomeTransactionRow(
+                    buttonColor: kcFailRed,
+                    buttonText: 'Failed',
+                    btc: '-0.001',
+                    image: bitCoin,
+                    imageBack: kcYellowBright,
+                    btcColor: kcYellowBright,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  HomeTransactionRow(
+                    buttonColor: kcPendingGrey,
+                    buttonText: 'Pending',
+                    btc: '-0.001',
+                    image: bitCoin,
+                    imageBack: kcYellowBright,
+                    btcColor: kcYellowBright,
+                  ),
+                ],
               ),
-              Container(
-                padding: const EdgeInsets.all(20.0),
-                child: const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '02 Dec 2023',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    HomeTransactionRow(
-                      buttonColor: kcSuccessGreen,
-                      buttonText: 'Completed',
-                      btc: '+0.001',
-                      image: bitCoin,
-                      imageBack: kcYellowBright,
-                      btcColor: kcYellowBright,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    HomeTransactionRow(
-                      buttonColor: kcFailRed,
-                      buttonText: 'Failed',
-                      btc: '-0.001',
-                      image: bitCoin,
-                      imageBack: kcYellowBright,
-                      btcColor: kcYellowBright,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    HomeTransactionRow(
-                      buttonColor: kcPendingGrey,
-                      buttonText: 'Pending',
-                      btc: '-0.001',
-                      image: silver,
-                      imageBack: kcSilverLight,
-                      btcColor: kcSilverLight,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    HomeTransactionRow(
-                      buttonColor: kcPendingGrey,
-                      buttonText: 'Pending',
-                      btc: '-0.001',
-                      image: silver,
-                      imageBack: kcSilverLight,
-                      btcColor: kcSilverLight,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      '02 Dec 2023',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    HomeTransactionRow(
-                      buttonColor: kcSuccessGreen,
-                      buttonText: 'Completed',
-                      btc: '+0.001',
-                      image: silver,
-                      imageBack: kcSilverLight,
-                      btcColor: kcSilverLight,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    HomeTransactionRow(
-                      buttonColor: kcFailRed,
-                      buttonText: 'Failed',
-                      btc: '-0.001',
-                      image: bitCoin,
-                      imageBack: kcYellowBright,
-                      btcColor: kcYellowBright,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    HomeTransactionRow(
-                      buttonColor: kcPendingGrey,
-                      buttonText: 'Pending',
-                      btc: '-0.001',
-                      image: bitCoin,
-                      imageBack: kcYellowBright,
-                      btcColor: kcYellowBright,
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
