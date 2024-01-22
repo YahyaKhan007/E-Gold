14d9d95a5d7890ec13bf8743be611afd92f6584f
UMChannar <usamamuhammadchannar@gmail.com>
2024-01-22 10:40:45 +0500

    Merged with komail

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -21,10 +21,6 @@ import 'package:e_gold/ui/views/kycidcardback/kycidcardback_view.dart';
 import 'package:e_gold/ui/views/kycbankaccount/kycbankaccount_view.dart';
 import 'package:e_gold/ui/views/kycprofile/kycprofile_view.dart';
 import 'package:e_gold/ui/views/kycpassport/kycpassport_view.dart';
-import 'package:e_gold/ui/views/dashboard_screen/dashboard_screen_view.dart';
-import 'package:e_gold/ui/views/transaction_history_screen/transaction_history_screen_view.dart';
-import 'package:e_gold/ui/views/notification_screen/notification_screen_view.dart';
-import 'package:e_gold/ui/views/account_screen/account_screen_view.dart';
 // @stacked-import
 
 @StackedApp(
@@ -42,17 +38,12 @@ import 'package:e_gold/ui/views/account_screen/account_screen_view.dart';
     MaterialRoute(page: ChangepasswordView),
     MaterialRoute(page: KycView),
     MaterialRoute(page: StatisticView),
-
     MaterialRoute(page: BuygoldorsilverView),
     MaterialRoute(page: KycidcardfrontView),
     MaterialRoute(page: KycidcardbackView),
     MaterialRoute(page: KycbankaccountView),
     MaterialRoute(page: KycprofileView),
     MaterialRoute(page: KycpassportView),
-    MaterialRoute(page: DashboardScreenView),
-    MaterialRoute(page: TransactionHistoryScreenView),
-    MaterialRoute(page: NotificationScreenView),
-    MaterialRoute(page: AccountScreenView),
 // @stacked-route
   ],
   dependencies: [
--- lib/ui/views/auth_views/login/login_viewmodel.dart
+++ lib/ui/views/auth_views/login/login_viewmodel.dart
@@ -1,6 +1,9 @@
 import 'package:e_gold/app/app.locator.dart';
 import 'package:e_gold/app/app.router.dart';
 import 'package:e_gold/services/auth_service.dart';
+import 'package:e_gold/services/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:e_gold/services/auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
@@ -27,14 +30,21 @@ class LoginViewModel extends BaseViewModel {
     rebuildUi();
   }
 
-  void onPressedLogin() {
-    navigationService.navigateToDashboardScreenView();
-    // if (formKey.currentState!.validate()) {
-    //   navigationService.replaceWithHomeView();
-    // }
   bool validateForm() {
     return formKey.currentState?.validate() ?? false;
+    // void onPressedLogin() {
+    //   navigationService.navigateToDashboardScreenView();
+    //   // if (formKey.currentState!.validate()) {
+    //   //   navigationService.replaceWithHomeView();
   }
+  // void onPressedLogin() {
+  //   navigationService.navigateToDashboardScreenView();
+  //   // if (formKey.currentState!.validate()) {
+  //   //   navigationService.replaceWithHomeView();
+  //   // }
+  // bool validateForm() {
+  //   return formKey.currentState?.validate() ?? false;
+  // }
 
   void onPressedLogin() async {
     if (validateForm()) {
@@ -42,7 +52,7 @@ class LoginViewModel extends BaseViewModel {
           emailController.text.trim(), passwordController.text.trim());
 
       if (user != null) {
-        navigationService.navigateToKycView();
+        navigationService.replaceWithDashboardScreenView();
       }
     }
   }
