9c2b2acb66dba7d81ea7add2f5f3330786f17c35
komail2105 <mkomailabbas21@gmail.com>
2024-01-24 11:23:31 +0500

    forget password updated

--- lib/ui/views/auth_views/signup/signup_viewmodel.dart
+++ lib/ui/views/auth_views/signup/signup_viewmodel.dart
@@ -1,31 +1,23 @@
 import 'package:e_gold/app/app.router.dart';
-import 'package:e_gold/services/auth_service.dart';
 import 'package:e_gold/ui/common/app_strings.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 import 'package:stacked/stacked.dart';
-
-import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:stacked_services/stacked_services.dart';
 
 import '../../../../app/app.locator.dart';
 
 class SignupViewModel extends BaseViewModel {
-  firebase_auth.FirebaseAuth firebaseAuth = firebase_auth.FirebaseAuth.instance;
-
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
   TextEditingController passwordController = TextEditingController();
   TextEditingController conformPasswordController = TextEditingController();
   TextEditingController emailController = TextEditingController();
-  TextEditingController nameController = TextEditingController();
   TextEditingController phoneController = TextEditingController();
   String initialCountry = 'US';
   PhoneNumber number = PhoneNumber(isoCode: 'US');
   bool isPasswordVisible = false;
   bool isConformPasswordVisible = false;
-  final _authService = locator<AuthService>();
+
   final navigationService = locator<NavigationService>();
 
   void showPassword() {
@@ -38,28 +30,37 @@ class SignupViewModel extends BaseViewModel {
     rebuildUi();
   }
 
+  void onPressedSignup() {
+    if (formKey.currentState!.validate()) {}
+  }
+
+  String? signUpEmailValidator(String? value) {
+    return !emailRegExp.hasMatch(value!)
+        ? 'Invalid email addres.'
+        : 'Please enter your email.';
+  }
+
+  String? signUpPasswordValidator(value) {
+    if (emailController.text.isEmpty) {
+      return 'Please enter your password';
+    }
+
+    return null;
+  }
+
+  String? signUpConfirmPasswordValidator(value) {
+    if (conformPasswordController.text.isEmpty) {
+      return 'Please enter confirm password';
+    }
+
+    return null;
+  }
+
   String? signUpPhoneValidator(String? value) {
     return null;
   }
 
-  bool validateForm() {
-    return formKey.currentState?.validate() ?? false;
-  }
-
   void onPressedAlreadyHaveAnAccount() {
     navigationService.replaceWithLoginView();
-  }
-
-  void onTapSignUp() async {
-    if (validateForm()) {
-      User? user = await _authService.signUpWithEmail(
-          emailController.text.trim(),
-          passwordController.text.trim(),
-          nameController.text);
-
-      if (user != null) {
-        navigationService.navigateToLoginView();
-      }
-    }
   }
 }
