403c857898bd38cfd3afec0849d12ee302fca6fa
komail2105 <mkomailabbas21@gmail.com>
2024-01-25 09:32:51 +0500

    forget password and statistic work

--- lib/ui/views/statistic/statistic_viewmodel.dart
+++ lib/ui/views/statistic/statistic_viewmodel.dart
@@ -1,5 +1,15 @@
+import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
 class StatisticViewModel extends BaseViewModel {
+  int? selectedValue = 1;
+
   void onButtonPressed() {}
+
+  void onButtonPressedDropDown(value) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      selectedValue = value;
+      rebuildUi();
+    });
+  }
 }
